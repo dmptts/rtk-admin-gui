77094cb39a6b3d747963ac4c84696982738c814f
Dmitriy Solovev <dmptts@gmail.com>
2022-12-01 17:30:04 +0800

    Реализует поиск по столбцам таблицы

--- src/components/ModelsTable.tsx
+++ src/components/ModelsTable.tsx
@@ -1,17 +1,53 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Table } from 'react-bootstrap';
 import { fetchModels } from '../api/models';
 import { useAppDispatch, useAppSelector } from '../hooks';
 import { selectors } from '../store/modelsSlice';
+import Search from './Search';
 import Model from './Model';
+import { Model as ModelInterface } from '../const';
+
+export type SearchStateI = {
+  [key in keyof ModelInterface]: string;
+};
 
 function ModelsTable () {
   const dispatch = useAppDispatch();
   const models = useAppSelector(selectors.selectAll);
+  const [searchState, setSearchState] = useState<SearchStateI | null>(null);
+
+  const filterModels = (model: ModelInterface) => {
+    if (searchState) {
+      const results = [];
+
+      for (const key in model) {
+        if (searchState[key as keyof SearchStateI] === '') {
+          results.push(true);
+        } else if (model[key as keyof SearchStateI].toString().toLowerCase().includes(searchState[key as keyof SearchStateI].toLowerCase())) {
+          results.push(true);
+        } else {
+          results.push(false);
+        };
+      };
+
+      return results.every((result) => result === true) && model;
+    };
+  };
 
   useEffect(() => {
     dispatch(fetchModels());
   }, [dispatch]);
+
+  useEffect(() => {
+    if (models.length > 0) {
+      setSearchState(
+        Object.keys(models[0]).reduce((state, key) => {
+          state[key as keyof SearchStateI] = '';
+          return state;  
+        }, {} as SearchStateI)
+      );
+    }
+  }, [models]);
 
   return (
     <Table>
@@ -27,7 +63,8 @@ function ModelsTable () {
         </tr>
       </thead>
       <tbody>
-        {models.map((model) => <Model key={model.id} model={model} />)}
+        {searchState && models.length > 0 && <Search state={searchState} stateSetter={setSearchState} />}
+        {models.filter((model) => filterModels(model)).map((model) => <Model key={model.id} model={model} />)}
       </tbody>
     </Table>
   );
---
+++ src/components/Search.tsx
@@ -0,0 +1,31 @@
+import React, { Dispatch } from 'react';
+
+interface SearchProps<T> {
+  state: T,
+  stateSetter: Dispatch<T>
+}
+
+interface SearchStateI {
+  [key: string]: string,
+}
+
+function Search<T extends SearchStateI> ({state, stateSetter}: SearchProps<T>) {
+  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    stateSetter({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <tr>
+      {Object.keys(state).map((param) => {
+        return <td>
+          <input type="text" name={param} value={state[param as keyof T]} onChange={changeHandler} />
+        </td>
+      })}
+    </tr>
+  )
+}
+
+export default Search;
