36ac7e6f68102b47ff3ee181a76e00769fa56d57
Dmitriy Solovev <dmptts@gmail.com>
2022-12-06 09:59:57 +0800

    Выносит эффекты и рендер в отдельные коллбэки

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -1,13 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Table } from 'react-bootstrap';
 import Search from './Search';
 import TablePagination from './TablePagination';
 import DataRow from './DataRow';
 import { AsyncThunk } from '@reduxjs/toolkit';
-
-export type SearchStateI<I> = {
-  [key in keyof I]: string;
-};
 
 interface DataTableProps<T> {
   data: T[],
@@ -22,70 +18,92 @@ function DataTable<T extends { id: number, [key: string]: any }> ({ data, patchE
   const [pagesCount, setPagesCount] = useState<number>(0);
   const [filteredData, setFilteredData] = useState<T[] | null>(null);
 
+  const getInitialSearchState = useCallback(() => {
+    setSearchState(
+      Object.keys(data[0]).reduce((state, key) => {
+        state[key as keyof T] = '';
+        return state;  
+      }, {} as {[key in keyof T]: string})
+    );
+  }, [data]);
+
+  const getTableHeadings = useCallback(() => {
+    return data && data.length > 0 && Object.entries(data[0]).map((entry, i) => <th key={i}>{entry[0]}</th>)
+  }, [data]);
+
+  const filterData = useCallback(() => {
+    setFilteredData(data.filter((entity) => {
+      if (searchState) {
+        const results = [];
+  
+        for (const key in entity) {
+          if (searchState[key] === '') {
+            results.push(true);
+          } else if (entity[key as keyof T].toString().toLowerCase().includes(searchState[key].toLowerCase())) {
+            results.push(true);
+          } else {
+            results.push(false);
+          };
+        };
+  
+        return results.every((result) => result === true) && entity;
+      } else {
+        return false;
+      }
+    }));
+  }, [data, searchState]);
+
+  const getPagesCount = useCallback(() => {
+    if (filteredData) {
+      setPagesCount(filteredData.length > itemsPerPage ? Math.ceil(filteredData.length / itemsPerPage): 0);
+    }
+  }, [filteredData]);
+
+  const renderData = useCallback(() => {
+    return filteredData && filteredData.map((entity, i) => {
+      if ((currentPage * itemsPerPage) - itemsPerPage < i + 1 && currentPage * itemsPerPage >= i + 1) {
+        return <DataRow<T>
+          key={entity.id}
+          entity={entity}
+          patchEntity={patchEntity}
+          deleteEntity={deleteEntity}
+        />
+      } else {
+        return null;
+      };
+    })
+  }, [currentPage, deleteEntity, filteredData, patchEntity])
+
   useEffect(() => {
     if (data.length > 0) {
-      setSearchState(
-        Object.keys(data[0]).reduce((state, key) => {
-          state[key as keyof T] = '';
-          return state;  
-        }, {} as {[key in keyof T]: string})
-      );
+      getInitialSearchState();
     }
-  }, [data]);
+  }, [data, getInitialSearchState]);
 
   useEffect(() => {
     if (data) {
-      setFilteredData(data.filter((entity) => {
-        if (searchState) {
-          const results = [];
-    
-          for (const key in entity) {
-            if (searchState[key] === '') {
-              results.push(true);
-            } else if (entity[key as keyof T].toString().toLowerCase().includes(searchState[key].toLowerCase())) {
-              results.push(true);
-            } else {
-              results.push(false);
-            };
-          };
-    
-          return results.every((result) => result === true) && entity;
-        } else {
-          return false;
-        }
-      }));
+      filterData();
     };
-  }, [data, searchState])
+  }, [data, searchState, filterData])
 
   useEffect(() => {
     if (filteredData) {
       setCurrentPage(1);
-      setPagesCount(filteredData.length > itemsPerPage ? Math.ceil(filteredData.length / itemsPerPage): 0);
+      getPagesCount();
     };
-  }, [filteredData])
+  }, [filteredData, getPagesCount])
 
   return (
     <>
       <Table>
         <thead>
           <tr>
-            {filteredData && filteredData.length > 0 && Object.entries(filteredData[0]).map((entry, i) => <th key={i}>{entry[0]}</th>)}
+            {getTableHeadings()}
           </tr>
         </thead>
         <tbody>
           {searchState && data.length > 0 && <Search state={searchState} stateSetter={setSearchState} />}
-          {filteredData && filteredData.map((entity, i) => {
-            if ((currentPage * itemsPerPage) - itemsPerPage < i + 1 && currentPage * itemsPerPage >= i + 1) {
-              return <DataRow<T>
-                key={entity.id}
-                entity={entity}
-                patchEntity={patchEntity}
-                deleteEntity={deleteEntity}
-              />
-            } else {
-              return null;
-            };
-          })}
+          {renderData()}
         </tbody>
       </Table>
       <TablePagination
